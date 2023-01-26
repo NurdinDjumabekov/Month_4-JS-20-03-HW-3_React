1116924f301e8b960d0025ee8ec7747e2a122ca9
Nurdin Djumabekov <dui.nurdin@gmail.com>
2023-01-27 01:01:19 +0600

    third commit

--- src/components/Foods/Foods.jsx
+++ src/components/Foods/Foods.jsx
@@ -1,24 +1,18 @@
-import { useState } from "react";
 import FoodCss from "./Foods.module.css";
 
 function Foods(product) {
-  // const [count, SetCount] = useState(1);
   const addBacket = (event) => {
     console.log(event.target);
     console.log("Код товара " + product.food.id);
     console.log(product.food.name);
     console.log(product.food.price);
     console.log(product.food.mass);
-    // console.log(product.food.url);
-    ////////////////////////////////////////////////////
-    // i = i + 1;
-    // console.log(i);
-    SetCount((count) => count + 1);
-    console.log(count);
-    alert(`
-      Вы заказали : ${product.food.name}
-      по цене : ${product.food.price}
-      `);
+    // console.log(product.num.count);
+
+    // alert(`
+    //   Вы заказали : ${product.food.name}
+    //   по цене : ${product.food.price}
+    //   `);
   };
 
   return (
